5d051e5125472fd788cc8451ef4694311cc3397d
sdenisen <sdenisen@ymail.com>
2016-09-22 20:27:42 +0300

    Added unittests for some sudoku methods.

--- euler-problem-96/unittests.py
+++ euler-problem-96/unittests.py
@@ -1,12 +1,39 @@
 __author__ = 'Sergey'
+
+from sudoku_solver import Sudoku
 import unittest
 
+
 class Test(unittest.TestCase):
+
+    def setUp(self):
+        str_sudoku = """003020600
+                        900305001
+                        001806400
+                        008102900
+                        700000008
+                        006708200
+                        002609500
+                        800203009
+                        005010300"""
+
+        int_sudoku = []
+        for line in str_sudoku.split("\n"):
+            int_sudoku.append([int(i) for i in list(line.strip())])
+        self.s = Sudoku(int_sudoku)
+
     def testColContent(self):
-        pass
+        result = self.s.colContent(0)
+        self.assertListEqual(result, [9, 7, 8], "wrong expected list.")
 
     def testRowContent(self):
-        pass
+        result = self.s.rowContent(0)
+        self.assertListEqual(result, [3, 2, 6], "wrong expected list.")
 
     def testSectContent(self):
-        pass
+        result = self.s.sectContent(0, 3)
+        self.assertListEqual(result, [], "wrong expected list.")
+
+
+if __name__ == "__main__":
+    unittest.main()
