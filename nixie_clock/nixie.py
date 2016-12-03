2b3b4f0686b100ec0d9818d37884cf57d9d545f0
sdenisen <sdenisen@ymail.com>
2016-12-04 00:00:38 +0300

    now it's working version for test nixie tubes

--- nixie_clock/nixie.py
+++ nixie_clock/nixie.py
@@ -1,3 +1,5 @@
+import unittest
+
 __author__ = 'Sergey'
 
 
@@ -9,4 +11,24 @@ def convertToBinaryArray(digit):
         """
         if digit > 9 or digit < 0:
             raise Exception("invalid passed parameter")
-        return [abs(int(i)-1) for i in '{0:0>4b}'.format(digit)]
+        return [abs(int(i)) for i in '{0:0>4b}'.format(digit)]
+
+
+class Tests (unittest.TestCase):
+
+    def test_convertToBinaryArray(self):
+        #  0 - 0000; 4 - 0100; 8 - 1000
+        #  1 - 0001; 5 - 0101; 9 - 1001
+        #  2 - 0010; 6 - 0110
+        #  3 - 0011; 7 - 0111
+        msg_str = "wrong convertions of %s"
+        expected_dict = [[0, 0, 0, 0], [0, 0, 0, 1],
+                         [0, 0, 1, 0], [0, 0, 1, 1], [0, 1, 0, 0],
+                         [0, 1, 0, 1], [0, 1, 1, 0], [0, 1, 1, 1],
+                         [1, 0, 0, 0], [1, 0, 0, 1]]
+        for i in range(9):
+            self.assertListEqual(expected_dict[i], convertToBinaryArray(i), msg_str%str(i))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- nixie_clock/nixie_clock.py
+++ nixie_clock/nixie_clock.py
@@ -2,38 +2,35 @@ import RPi.GPIO as GPIO
 import time
 from nixie import convertToBinaryArray
 
-__author__ = 'Sergey'
-
-
 GPIO.setmode(GPIO.BOARD)
 
-channels_nixie = [3, 5, 7, 11, 13, 15]
-channels_digits = [40, 38, 36, 32]  # 8 - 4 - 2 - 1
+__author__ = 'Sergey'
 
-GPIO.setup(channels_nixie, GPIO.OUT)
-GPIO.setup(channels_digits, GPIO.OUT)
+class NixieClock():
+    channels_nixie = [3, 5, 7, 11, 13, 15]
+    channels_digits = [40, 38, 36, 32]  # 8 - 4 - 2 - 1
+    def setupPins(self):
+        GPIO.setup(self.channels_nixie, GPIO.OUT)
+        GPIO.setup(self.channels_digits, GPIO.OUT)
 
+    def resetNixiePins(self):
+        GPIO.output( self.channels_nixie, map(int, str(GPIO.LOW)*len(self.channels_nixie)) )
 
-for channel in channels_nixie:
-    GPIO.output(channel, GPIO.LOW)
+    def resetChannelPins(self):
+            GPIO.output( self.channels_digits, map(int, str(GPIO.LOW)*len(self.channels_digits)) )
 
-for channel in channels_digits:
-    GPIO.output(channel, GPIO.LOW)
+nc = NixieClock()
 
-while True:
-    for channel in channels_nixie:
-        GPIO.output(channel, GPIO.HIGH)
-        for digit in range(9):
-            b_array = convertToBinaryArray(digit)
-            for i in range(4):
-                GPIO.output(channels_digits[i], b_array[i])
-            time.sleep(1)
+nc.setupPins()
+nc.resetChannelPins()
+nc.resetNixiePins()
 
-            for channel in channels_digits:
-                GPIO.output(channel, GPIO.LOW)
-
+for channel in nc.channels_nixie:
+    GPIO.output(channel, GPIO.HIGH)
+    for i in range(10):
+        b_array = convertToBinaryArray(i)
+        print "digit = %s - %s" % (i, b_array)
+        GPIO.output(nc.channels_digits, b_array)
         time.sleep(1)
-        GPIO.output(channel, GPIO.LOW)
-
-        for channel in channels_nixie:
-            GPIO.output(channel, GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(channel, GPIO.LOW)
