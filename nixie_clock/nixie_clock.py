a70a865fce3d826c9beb56de680d728801e81c61
sdenisen <sdenisen@ymail.com>
2016-12-04 23:18:36 +0300

    completed version 1.0

--- nixie_clock/nixie_clock.py
+++ nixie_clock/nixie_clock.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
-import time
-from nixie import NixieTube
+from socket import AF_INET, SOCK_DGRAM
+import socket
+import struct, time
+
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -8,43 +10,84 @@ __author__ = 'Sergey'
 
 
 class NixieClock(object):
-    channels_tube_sec = [13, 15]
-    channels_tube_hm = [3, 5, 7, 11]
-    channels_digits = [40, 38, 36, 32]  # 8 - 4 - 2 - 1
+    CHANNEL_HOUR_0 = 3
+    CHANNEL_HOUR_1 = 5
+    CHANNEL_MINUTE_0 = 7
+    CHANNEL_MINUTE_1 = 11
+    CHANNEL_SECOND_0 = 13
+    CHANNEL_SECOND_1 = 15
+    CHANNEL_SECONDS = [CHANNEL_SECOND_0, CHANNEL_SECOND_1]
+    CHANNEL_HOURS = [CHANNEL_HOUR_0, CHANNEL_HOUR_1]
+    CHANNEL_MINUTES = [CHANNEL_MINUTE_0, CHANNEL_MINUTE_1]
 
-    def setupPins(self):
-        GPIO.setup(self.channels_tube_hm, GPIO.OUT)
-        GPIO.setup(self.channels_tube_sec, GPIO.OUT)
+    channels_digits = [40, 38, 36, 32]  # 8 - 4 - 2 - 1
+    TUBE_TYPE_SECONDS = "TUBE_TYPE_SECONDS"
+    TUBE_TYPE_HOURS_MINUTES = "TUBE_TYPE_HOURS_MINUTES"
+
+    def __init__(self):
+        GPIO.setup(self.CHANNEL_HOURS + self.CHANNEL_MINUTES + self.CHANNEL_SECONDS, GPIO.OUT)
         GPIO.setup(self.channels_digits, GPIO.OUT)
 
-    def resetNixiePins(self):
-        GPIO.output(self.channels_tube_hm, map(int, str(GPIO.LOW) * len(self.channels_tube_hm)))
-        GPIO.output(self.channels_tube_sec, map(int, str(GPIO.LOW) * len(self.channels_tube_sec)))
+    def _show(self, d_digit, tube_type):
+        if d_digit > 9 or d_digit < 0:
+            raise Exception("invalid passed parameter")
 
-    def resetChannelPins(self):
-        GPIO.output(self.channels_digits, map(int, str(GPIO.LOW) * len(self.channels_digits)))
+        digit_to_show = d_digit
+        if tube_type == self.TUBE_TYPE_HOURS_MINUTES:
+            convert_digit = {0:2, 1:1, 2:0, 3:9, 4:8, 5:7, 6:6, 7:5, 8:4, 9:3}
+            digit_to_show = convert_digit[d_digit]
+
+        b_array = [abs(int(i)) for i in '{0:0>4b}'.format(digit_to_show)]
+        GPIO.output(self.channels_digits, b_array)
+
+    def _convertToDigitArray(self, number):
+        digits = []
+        while True:
+            d = number%10
+            digits.append(d)
+            number = (number - d)/10
+            if number<=9:
+                digits.append(number)
+                break
+        return list(reversed(digits))
+
+    def _getNTPTime(self, host="pool.ntp.org"):
+        port = 123
+        buf = 1024
+        address = (host, port)
+        msg = '\x1b' + 47 * '\0'
+
+        # reference time (in seconds since 1900-01-01 00:00:00)
+        TIME1970 = 2208988800L  # 1970-01-01 00:00:00
+
+        # connect to server
+        client = socket.socket(AF_INET, SOCK_DGRAM)
+        client.sendto(msg, address)
+        msg, address = client.recvfrom(buf)
+
+        t = struct.unpack("!12I", msg)[10]
+        t -= TIME1970# - 3600 * 3
+        return time.gmtime(t)
+
+
+    def run(self):
+        while True:
+            struct_time = time.localtime() #(tm_year,tm_mon,tm_mday,tm_hour,tm_min, tm_sec,tm_wday,tm_yday,tm_isdst)
+            hours = self._convertToDigitArray(struct_time[3])
+            minutes = self._convertToDigitArray(struct_time[4])
+            seconds = self._convertToDigitArray(struct_time[5])
+            for channel, digit in zip(self.CHANNEL_HOURS + self.CHANNEL_MINUTES, hours + minutes):
+                GPIO.output(channel, GPIO.HIGH)
+                self._show(digit, self.TUBE_TYPE_HOURS_MINUTES)
+                time.sleep(0.005)
+                GPIO.output(channel, GPIO.LOW)
+
+            for channel, digit in zip(self.CHANNEL_SECONDS, seconds):
+                GPIO.output(channel, GPIO.HIGH)
+                self._show(digit, self.TUBE_TYPE_SECONDS)
+                time.sleep(0.002)
+                GPIO.output(channel, GPIO.LOW)
 
 
 nc = NixieClock()
-nt_h = NixieTube(NixieTube.TUBE_TYPE_HOURS_MINUTES, nc.channels_digits)
-nt_s = NixieTube(NixieTube.TUBE_TYPE_SECONDS, nc.channels_digits)
-
-nc.setupPins()
-nc.resetChannelPins()
-nc.resetNixiePins()
-
-for channel in nc.channels_tube_hm:
-    GPIO.output(channel, GPIO.HIGH)
-    for i in range(10):
-        nt_h.show(i)
-        time.sleep(1)
-    time.sleep(1)
-    GPIO.output(channel, GPIO.LOW)
-
-for channel in nc.channels_tube_sec:
-    GPIO.output(channel, GPIO.HIGH)
-    for i in range(10):
-        nt_s.show(i)
-        time.sleep(1)
-    time.sleep(1)
-    GPIO.output(channel, GPIO.LOW)
+nc.run()
