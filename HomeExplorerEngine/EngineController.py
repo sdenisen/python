76538863411208ab1ef198e11586d3f3f9447c7f
sdenisen <sdenisen@ymail.com>
2017-06-17 23:59:23 +0300

    all of wheels works well :)

--- HomeExplorerEngine/EngineController.py
+++ HomeExplorerEngine/EngineController.py
@@ -1,15 +1,40 @@
 __author__ = 'sdeni'
 
+from HomeExplorerEngine.Motor import Motor
+from HomeExplorerEngine.common_consts import *
+
 
 class EngineController(object):
+
     def __init__(self):
-        pass
+        self.motor_ahead_left = Motor(PIN_AHEAD_LEFT_FORWARD, PIN_AHEAD_LEFT_BACKWARD, PIN_AHEAD_LEFT_PWM, "ahead left")
+        self.motor_ahead_right = Motor(PIN_AHEAD_RIGHT_FORWARD, PIN_AHEAD_RIGHT_BACKWARD, PIN_AHEAD_RIGHT_PWM, "ahead right")
+
+        self.motor_behind_right = Motor(PIN_BEHIND_RIGHT_FORWARD, PIN_BEHIND_RIGHT_BACKWARD, PIN_BEHIND_RIGHT_PWM, "behind right")
+        self.motor_ahead_left.start()
+        self.motor_behind_right.start()
+
+        self.motor_behind_left = Motor(PIN_BEHIND_LEFT_FORWARD, PIN_BEHIND_LEFT_BACKWARD, PIN_BEHIND_LEFT_PWM, "behind left")
+        self.motor_ahead_right.start()
+        self.motor_behind_left.start()
+
 
     def goForward(self):
-        pass
+        print ("launch FORWARD action...")
+        self.motor_ahead_left.event_forward.set()
+        self.motor_ahead_right.event_forward.set()
+        self.motor_behind_right.event_forward.set()
+        self.motor_behind_left.event_forward.set()
+        print ("set events")
+
 
     def goBackward(self):
-        pass
+        print ("launch BACKWARD action...")
+        self.motor_ahead_left.event_backward.set()
+        self.motor_ahead_right.event_backward.set()
+        self.motor_behind_right.event_backward.set()
+        self.motor_behind_left.event_backward.set()
+        print ("set events")
 
     def goLeft(self):
         pass
@@ -18,7 +43,18 @@ class EngineController(object):
         pass
 
     def stop(self):
-        pass
+        print ("launch STOP action...")
+        self.motor_ahead_left.event_stop.set()
+        self.motor_ahead_right.event_stop.set()
+        self.motor_behind_right.event_stop.set()
+        self.motor_behind_left.event_stop.set()
+        print ("set events")
 
     def calibrate(self):
         pass
+
+    def cleanup(self):
+        self.motor_ahead_left.kill()
+        self.motor_ahead_right.kill()
+        self.motor_behind_left.kill()
+        self.motor_behind_right.kill()
---
+++ HomeExplorerEngine/runner.py
@@ -0,0 +1,19 @@
+import time
+from HomeExplorerEngine.EngineController import EngineController
+
+__author__ = 'sdeni'
+
+ec = EngineController()
+
+ec.goForward()
+time.sleep(5)
+ec.stop()
+time.sleep(5)
+ec.goBackward()
+time.sleep(5)
+
+
+ec.stop()
+time.sleep(1)
+print "stop - all - motors"
+ec.cleanup()
