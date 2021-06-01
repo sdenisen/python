4614e5da6c91f9780bcb2024870dbc035c87034b
sdenisenko <sdenisen@ymail.com>
2021-06-01 17:51:28 +0300

    refactor: use functions;

--- yandex/task4/task4.py
+++ yandex/task4/task4.py
@@ -2,16 +2,26 @@ from urllib import parse
 import requests
 import json
 
-url = "http://127.0.0.1"
-port = 7777
-a = 2
-b = 4
-query_args = {'a': a, 'b': b}
-encoded_args = parse.urlencode(query_args)
-full_url = f"{url}:{port}?{encoded_args}"
-response_json = requests.get(full_url).text
-arr = json.loads(response_json)
-arr.sort()
-for i in arr:
-    print(i)
 
+def handle_cgi_request(url, port, param_a, param_b):
+    query_args = {'a': param_a, 'b': param_b}
+    encoded_args = parse.urlencode(query_args)
+    full_url = f"{url}:{port}?{encoded_args}"
+    response_json = requests.get(full_url).text
+    arr = json.loads(response_json)
+    arr.sort()
+    return arr
+
+
+def main():
+    url = input()
+    port = input()
+    a = input()
+    b = input()
+    arr = handle_cgi_request(url, port, a, b)
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
