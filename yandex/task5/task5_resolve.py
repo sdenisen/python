e4ca95d172c86903cb2848e649da42f9d6025d2f
sdenisenko <sdenisen@ymail.com>
2021-06-01 17:06:23 +0300

    refactor: add some missed changes;

--- yandex/task5/task5_resolve.py
+++ yandex/task5/task5_resolve.py
@@ -7,7 +7,7 @@ def initData():
     data = input().split(" ")
     user_limit = int(data[0])
     service_limit = int(data[1])
-    duration = float(data[2]) / 1000
+    duration = int(data[2])
     return user_limit, service_limit, duration
 
 
@@ -18,40 +18,48 @@ class RequestCollector:
         self.service_limit = service_limit
         self.duration = duration
         self.dict_users = defaultdict(list)  # key = user_id, value = [request_time1, request_time2, ...]
-        self.count_service_requests = 0
+        self.request_times = list()
 
-    def check_timeouts(self, user_requests_time, duration, src):
+    def check_timeouts(self, user_id, new_request):
         r = []
-        for request_time in user_requests_time:
-            if request_time < time.time() - duration:
-                src -= 1
+        for request_time in self.dict_users[user_id]:
+            if request_time < new_request - self.duration:
                 continue
             r.append(request_time)
-        return r, src
+        return r
+
+    def check_request_times(self, new_request):
+        r = list()
+        for time in self.request_times:
+            if time < new_request - self.duration:
+                continue
+            r.append(time)
+        return r
 
     def get_request_status(self, str_request_data):
         request = str_request_data.split(" ")
-        user_id = request[1]
         request_time = int(request[0])
+        user_id = request[1]
 
-        user_requests = self.dict_users[user_id]
-        result, count = self.check_timeouts(user_requests, self.duration, self.count_service_requests)
-        count_service_requests = count
-        if len(result) > self.user_limit - 1:
+        r = self.check_timeouts(user_id, request_time)
+        if len(r) > self.user_limit - 1:
             return 429
 
-        if count_service_requests > self.service_limit - 1:
+        r_service_requests = self.check_request_times(new_request=request_time)
+        if len(r_service_requests) > self.service_limit - 1:
             return 503
 
         # add user to dict:
-        result.append(request_time)
-        self.dict_users[user_id] = result
-        count_service_requests += 1
+        r.append(request_time)
+        r_service_requests.append(request_time)
+
+        # add time to request times list:
+        self.dict_users[user_id] = r
+        self.request_times = r_service_requests
         return 200
 
 
 def main():
-
     user_limit, service_limit, duration = initData()
 
     rc = RequestCollector(user_limit, service_limit, duration)
@@ -64,8 +72,7 @@ def main():
         print(status)
         stdout.flush()
 
-print(__file__)
-print(__name__)
+
 if __name__ == "__main__":
     main()
 
--- yandex/task5/task5_tests.py
+++ yandex/task5/task5_tests.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 import task5.task5_resolve
@@ -5,11 +6,27 @@ import task5.task5_resolve
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        rc = task5.task5_resolve.RequestCollector(user_limit=3, service_limit=5, duration=0.0001)
-        verify_data = [(1, 100, 200), (1, 100, 200), (1, 100, 200), (1, 100, 200), (1, 100, 200)]
-        for user_id, request_time, responce in verify_data:
-            r = rc.get_request_status(f"{user_id} {request_time}")
-        self.assertEqual(r, responce)
+        rc = task5.task5_resolve.RequestCollector(user_limit=2, service_limit=5, duration=5)
+
+        verify_data = [(1, 100, 200),  # 1
+                       (1, 100, 200),  # 2
+                       (2, 100, 429),  # 3
+                       (2, 200, 200),  # 4
+                       (2, 300, 200),  # 5
+                       (2, 400, 200),  # 6
+                       (2, 500, 503),  # 7
+                       (3, 500, 503),  # 8
+                       (5, 200, 503),  # 9
+                       (6, 100, 429),  # 10
+                       (7, 200, 200)]  # 11
+        i = 1
+        for request_time, user_id, responce in verify_data:
+            r = rc.get_request_status(f"{request_time} {user_id}")
+            print(f"user_id: {user_id} request_time: {request_time} -> response: {r}; {i} -> {rc.dict_users} ===> {rc.request_times}")
+            i += 1
+            self.assertEqual(r, responce)
+            # print(rc.dict_users)
+
 
 
 
